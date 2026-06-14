1541d84bc888427899439019ce47b8111af8dea1
Artizmo <bezedulce@gmail.com>
2026-06-13 23:35:42 -0500

    refactored events, removed eventsmanager

--- src/lib/effects/poison.ts
+++ src/lib/effects/poison.ts
@@ -7,7 +7,6 @@ export const poisonEffect: Effect = {
   tick({ activeEffect, character }) {
     if (character.isDead) return;
 
-    console.log('poison tick')
     if (activeEffect.duration % activeEffect.interval === 0) {
       character.damage(5);
       Log.CHAR.INFO(`${character.name} takes 5 poison damage.`);
